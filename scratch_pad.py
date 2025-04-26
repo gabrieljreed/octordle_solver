7facd54b6012902a028cdda8453153b5f41a2e05
gabrieljreed <gabrieljreed@gmail.com>
2025-04-26 12:36:36 -0700

    Update scratch pad

--- scratch_pad.py
+++ scratch_pad.py
@@ -1,27 +1,14 @@
-from octordle_solver.dictionary import Dictionary
-from octordle_solver.generate_groups import (
-    get_best_word_groups,
-    print_group_info,
-    get_best_word_groups_parallel,
-    get_all_answer_possibilities,
-)
-from octordle_solver.solver import filter_words
-from octordle_solver.utils import clear_screen
+"""Scratch pad for trying things out."""
 
+from octordle_solver.dictionary import Dictionary
+from octordle_solver.solver import filter_words
 
 if __name__ == "__main__":
-    remaining_words = Dictionary().words.copy()
-    correct_letters = ["", "", "A", "", ""]
-    misplaced_letters = [("R", 1), ("E", 4)]
-    incorrect_letters = ["C", "N"]
+    remaining_words = Dictionary().valid_answers.copy()
+    correct_letters = ["", "", "", "", ""]
+    misplaced_letters = [("E", 4), ("S", 4), ("T", 3)]
+    incorrect_letters = ["C", "R", "A", "N", "D", "O", "L"]
     remaining_words = filter_words(remaining_words, correct_letters, misplaced_letters, incorrect_letters)
     for word in remaining_words:
         print(word)
     print(len(remaining_words))
-
-    possibilities = get_all_answer_possibilities(remaining_words)
-    for possibility in possibilities:
-        print(possibility)
-    print(len(possibilities))
-    # best_group, best_word = get_best_word_groups_parallel(remaining_words)
-    # print_group_info(best_group, best_word)
